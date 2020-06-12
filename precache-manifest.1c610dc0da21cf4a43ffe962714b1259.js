5590d3ed597a317b2c76de6b9f49c3b14df9ae11
truongnc1997 <nguyentruong221097@gmail.com>
2020-06-13 05:09:35 +0700

    Updates

---
+++ precache-manifest.1c610dc0da21cf4a43ffe962714b1259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179406798a0c6d5f8e949a6c1d5a07c4",
+    "url": "/deploy-git/index.html"
+  },
+  {
+    "revision": "0c6871b4cf9c838c37ff",
+    "url": "/deploy-git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "24acc32fdd904752fb07",
+    "url": "/deploy-git/static/js/2.8d56a7c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-git/static/js/2.8d56a7c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6871b4cf9c838c37ff",
+    "url": "/deploy-git/static/js/main.960a30bd.chunk.js"
+  },
+  {
+    "revision": "cfb8300d4de713f45573",
+    "url": "/deploy-git/static/js/runtime-main.e95ec2b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-git/static/media/logo.5d5d9eef.svg"
+  }
+]);
